0fb7a8a63ce30dbe1ff167c2393e6e3c03401beb
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-01-25 01:48:28 +0100

    added dashboard tests for connection

--- river-monitoring-service/app/src/main/java/rivermonitoringservice/WebSocketVerticle.java
+++ river-monitoring-service/app/src/main/java/rivermonitoringservice/WebSocketVerticle.java
@@ -9,6 +9,7 @@ class WebSocketVerticle extends AbstractVerticle {
 
     private HttpServer server;
     private int i = 1;
+    private int action = 0;
 
     @Override
     public void start() {
@@ -23,20 +24,50 @@ class WebSocketVerticle extends AbstractVerticle {
             ctx.textMessageHandler((msg) -> {
                 System.out.println("Server " + msg);
                 JsonObject item = new JsonObject();
-                String type = "RILEVATION";
-                long time = i;
-                double waterLevel = i;
-                i++;
-                System.out.println(i);
-                JsonObject rilevation = new JsonObject();
-                rilevation.put("time", time);
-                rilevation.put("waterLevel", waterLevel);
-                item.put("type", type);
-                item.put("data", rilevation);
-                    vertx.setTimer(3000, timerId -> {
-                        ctx.writeTextMessage(item.encode());
+                if (action % 4 == 0) {
+                    String type = "RILEVATION";
+                    long time = i;
+                    double waterLevel = i;
+                    i++;
+                    // System.out.println(i);
+                    JsonObject rilevation = new JsonObject();
+                    rilevation.put("time", time);
+                    rilevation.put("waterLevel", waterLevel);
+                    item.put("type", type);
+                    item.put("data", rilevation);
+                } else if (action % 4 == 1) {
+                    String type = "MODE";
+                    item.put("type", type);
+                    String mode = "MANUAL";
+                    item.put("mode", mode);
+
+                    // disable manual
+                    mode = "AUTOMATIC";
+                    JsonObject item2 = new JsonObject();
+                    item2.put("type", type);
+                    item2.put("mode", mode);
+                    vertx.setTimer(15000, timerId -> {
+                        ctx.writeTextMessage(item2.encode());
                     });
+                } else if (action % 4 == 2) {
+                    String type = "VALVE_OPENING";
+                    item.put("type", type);
+                    int valveOpening = 25;
+                    item.put("opening", valveOpening);
+                } else if (action % 4 == 3) {
+                    String type = "STATE";
+                    item.put("type", type);
+                    int valveOpening = 75;
+                    item.put("opening", valveOpening);
+                    String state = "ALARM_TOO_LOW";
+                    item.put("state", state);
+                }
+                
+                action++;
+                vertx.setTimer(3000, timerId -> {
+                    ctx.writeTextMessage(item.encode());
                 });
+            });
         }).listen(8080);
 
     }
