05da9591e3702b94a491eec818abe8090d481f8c
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-01-25 01:48:13 +0100

    added javascript packet types

--- river-monitoring-dashboard/websocket.js
+++ river-monitoring-dashboard/websocket.js
@@ -1,59 +1,99 @@
 'use strict';
 const RILEVATION = 'RILEVATION';
-const MODE_CHANGED = 'MODE';
+const MODE = 'MODE';
+const VALVE_OPENING = 'VALVE_OPENING';
 const MANUAL_MODE = 'MANUAL';
-const HIGH_WATER_LEVEL_MODE = 'HIGH_WATER_LEVEL';
-const FREE_MODE = 'FREE';
+const AUTOMATIC_MODE = 'AUTOMATIC';
+const STATE = 'STATE';
 const N_MIN = 0.5;
+
+// const NORMAL_STATE = 'NORMAL';
+// const ALARM_TOO_LOW_STATE = 'ALARM_TOO_LOW';
+// const PRE_ALARM_TOO_HIGH_STATE = 'PRE_ALARM_TOO_HIGH';
+// const ALARM_TOO_HIGH_STATE = 'ALARM_TOO_HIGH';
+// const ALARM_TOO_HIGH_CRITIC_STATE = 'ALARM_TOO_HIGH_CRITIC';
+
 const retryDelay = 5000;
 let exampleSocket;
-let dataList;
-let isManualMode = false;
+let dataList = [];
 let myLineChart;
 
 function connectWebSocket() {
     console.log("FUNZIONE WEB SOCKET CONNECT");
     exampleSocket = new WebSocket("ws://localhost:8080");
     dataList = [];
-    graphInit();
     
     exampleSocket.onmessage = (event) => {
         // Parse JSON data received from the server
         const jsonPacket = JSON.parse(event.data);
         const type = jsonPacket.type;
-        const jsonData = jsonPacket.data;
         if (type === RILEVATION) {
-            addRilevationData(jsonData);
+            addRilevationData(jsonPacket);
             console.log(dataList);
-        } else if (type === 'MODE') {
-            changeMode();
+        } else if (type === MODE) {
+            changeMode(jsonPacket);
+        } else if (type === VALVE_OPENING) {
+            changeValveOpening(jsonPacket);
+        } else if (type === STATE) {
+            changeState(jsonPacket);
         }
         // Wait for 3 seconds before sending a response
-        setTimeout(() => {
-            // Convert the object to JSON and send it to the server
-            exampleSocket.send("PROVA");
-        }, 3000);
+        // setTimeout(() => {
+        //     // Convert the object to JSON and send it to the server
+        //     exampleSocket.send("PROVA");
+        // }, 3000);
     };
     
+    document.getElementById("submitBtn").onclick = () => {
+        const valveOpening = document.getElementById("valve-opening-input").value;
+        exampleSocket.send(JSON.stringify({
+            valveOpening: valveOpening
+        }));
+    };
+
     exampleSocket.onclose = (event) => {
         console.error(`WebSocket closed with code ${event.code}. Reconnecting in ${retryDelay / 1000} seconds.`);
         setTimeout(connectWebSocket, retryDelay);
     };
 }
 
-function addRilevationData(jsonData) {
+function addRilevationData(jsonPacket) {
+    const rilevation = jsonPacket.data;
     console.log("FUNZIONE ADD RILEVATION DATA");
-    if (jsonData.time - dataList[0].time > N_MIN * 60 * 1000) {
+    while (dataList.length > 0 && rilevation.time - dataList[0].time > N_MIN * 60 * 1000) {
         // Rimuovi il primo elemento della lista
         dataList.shift();
     }
     // Add JSON object to dataList
-    dataList.push(jsonData);
+    dataList.push(rilevation);
     // Aggiorna il dataset del grafico
     myLineChart.data.labels = dataList.map(item => item.time);
     myLineChart.data.datasets[0].data = dataList.map(item => item.waterLevel);
     // Aggiorna il grafico
     myLineChart.update();
+}
+
+function changeMode(jsonPacket) {
+    const mode = jsonPacket.mode;
+    console.log("FUNZIONE CHANGE MODE");
+    if (mode === MANUAL_MODE) {
+        document.getElementById("alert").innerHTML = "Manual mode on";
+        document.getElementById("submitBtn").disabled = true;
+    } else if (mode === AUTOMATIC_MODE) {
+        document.getElementById("alert").innerHTML = "";
+        document.getElementById("submitBtn").disabled = false;
+    }
+}
+
+function changeValveOpening(jsonPacket) {
+    console.log("FUNZIONE CHANGE VALVE OPENING");
+    document.getElementById("valve-opening").innerHTML = jsonPacket.opening + "%";
+}
+
+function changeState(jsonPacket) {
+    console.log("FUNZIONE CHANGE STATE");
+    document.getElementById("state").innerHTML = jsonPacket.state;
+    changeValveOpening(jsonPacket);
 }
 
 function graphInit() {
@@ -80,9 +120,5 @@ function graphInit() {
     });
 }
 
-function changeMode() {
-}
-
-
-
+graphInit();
 connectWebSocket();
